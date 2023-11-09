394ae3fb52fd4a204c6595bdd6461a07d8ae06ae
Anurag Dutta <imanurag18@gmail.com>
2023-11-09 21:26:43 +0530

    changed

--- src/component/Basics/Shop.jsx
+++ src/component/Basics/Shop.jsx
@@ -3,26 +3,45 @@ import './style.css';
 import Menu from './menuApi';
 import MenuCard from './ShopCard';
 import Footer from './Footer';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Shop = () => {
     const [menuData, setMenuData] = React.useState(Menu);
-
+    
     const filterItem = (category) => {
         const updatedList = Menu.filter((curElem) => {
             return curElem.category === category;
         });
         setMenuData(updatedList);
     };
-
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
     return (
     <>
-        <div className='start'>Welcome to a brand new type of storefront, Anushoopy</div>
-        <nav className='navbar'>
-            <div className='btn-group'>
+        { isAuthenticated ? (
+            
+            <div className='auth'>
+                <div className='name'>Hey {user.name}, isn't it a Great Day to start off with your Shopping???</div>
+                <button className='log' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                    Log Out
+                </button>
+            </div>
+        ) : (
+            <div className='auth'>
+                <button className='log' onClick={() => loginWithRedirect()}>Log In</button>
+            </div>
+        )}      
+       
+        <div className='start'>
+            Welcome to a brand new type of storefront, Anushoopy        
+        </div>
+        <nav className='navbar'>        
+            <div className='btn-group'>  
+                                            
                 <button className='btn-group__item' onClick={() => filterItem("Summer Outfits")}>AnuSummer Outfits</button>
                 <button className='btn-group__item' onClick={() => filterItem("Winter Outfits")}>AnuWinter Outfits</button>
                 <button className='btn-group__item' onClick={() => filterItem("Traditional Outfits")}>AnuTraditional Outfits</button>
                 <button className='btn-group__item' onClick={() => setMenuData(Menu)}>All</button>
+                
             </div>
         </nav>
         <MenuCard menuData={menuData}/>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="anurag-tenant.us.auth0.com"
+      clientId="pdYI12ZHoWkYfdQBeLW4Yf2PXthmoRIZ"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
